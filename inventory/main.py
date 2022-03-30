c83d2af7740049a767b0b338f6f9f8dd2bbd8e12
nojoven <work.cedricjoseph@gmail.com>
2022-03-30 16:07:12 +0200

    feat: prevent future CORS troubles

--- inventory/main.py
+++ inventory/main.py
@@ -1,8 +1,16 @@
 from redis_om import get_redis_connection, HashModel
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from setup import REDIS_DB
 
 app = FastAPI()
+
+app.add_middleware(
+   CORSMiddleware,
+   allow_origins=["http://localhost:3000"],
+   allow_methods=["*"],
+   allow_headers=["*"],
+)
 
 redis = get_redis_connection(
    host="redis-15098.c299.asia-northeast1-1.gce.cloud.redislabs.com",
