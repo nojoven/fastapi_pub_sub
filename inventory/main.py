5e0d5d3bf30e462d94263f2e53582c22b59bb5b6
nojoven <work.cedricjoseph@gmail.com>
2022-03-30 15:27:53 +0200

    feat: add connection to redis db

--- inventory/main.py
+++ inventory/main.py
@@ -1,7 +1,15 @@
+from redis_om import get_redis_connection
 from fastapi import FastAPI
+from setup import REDIS_DB
 
 app = FastAPI()
 
+redis = get_redis_connection(
+   host=REDIS_DB,
+   port=15098,
+   password="",
+   decode_responses=True
+)
 
 @app.get("/")
 async def root():
