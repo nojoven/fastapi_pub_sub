c740c23fc05ba0a34d30ce3635a7a9217821e5c0
nojoven <work.cedricjoseph@gmail.com>
2022-03-30 15:47:43 +0200

    fix: fix hostname and password; feat: add an endpoint for products

--- inventory/main.py
+++ inventory/main.py
@@ -1,16 +1,26 @@
-from redis_om import get_redis_connection
+from redis_om import get_redis_connection, HashModel
 from fastapi import FastAPI
 from setup import REDIS_DB
 
 app = FastAPI()
 
 redis = get_redis_connection(
-   host=REDIS_DB,
+   host="redis-15098.c299.asia-northeast1-1.gce.cloud.redislabs.com",
    port=15098,
-   password="",
+   password=REDIS_DB,
    decode_responses=True
 )
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+class Product(HashModel):
+   name: str
+   price: float
+   available_quantity: int
+
+   class Meta:
+      database = redis 
+
+
+
+@app.get("/products")
+def all():
+   return Product.all_pks()
