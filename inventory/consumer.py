337d2c9fe1a5c25b00dad27cc384cda899c2edf3
nojoven <work.cedricjoseph@gmail.com>
2022-03-31 16:48:58 +0200

    feat: add subscriber, start consuming

---
+++ inventory/consumer.py
@@ -0,0 +1,29 @@
+import time
+from main import redis, Product
+
+key = 'order_completed'
+group = 'inventory-group'
+
+
+try:
+   redis.xgroup_create(key, group)
+except:
+   print("Group already exists !")
+
+while True:
+   try:
+      results = redis.xreadgroup(group, key, {key: '>'}, None)
+
+      if results != []:
+         for result in results:
+            obj = result[1][0][1]
+            product = Product.get(obj['product_id'])
+            print(product)
+            if product:
+               product.available_quantity = product.available_quantity - int(obj['quantity'])
+               product.save()
+
+   except Exception as e:
+      print(str(e))
+   
+   time.sleep(1)
